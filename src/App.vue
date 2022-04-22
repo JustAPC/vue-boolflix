f5c8f6e24da806387f3ff10b7a7ab4b4d0f3b33a
Andrea Pontillo <andreapontillo97@gmail.com>
2022-04-22 14:44:05 +0200

    prova

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,9 @@ export default {
   },
 
   created() {
-    axios
+    multimediaFiltering (){
+      if (this.testoRicerca === '') {
+        axios
       .get(
         `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=ritorno al futuro`
       )
@@ -38,6 +40,20 @@ export default {
         this.multimediaList = res.data.results;
         console.log(this.multimediaList);
       });
+      }
+
+      else {
+        axios
+      .get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${this.apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+      )
+      .then((res) => {
+        this.multimediaList = res.data.results;
+        console.log(this.multimediaList);
+      });
+      }
+    }
+
   },
 };
 </script>
