982cfad2c9d4a6f865bd286acf9155941dcc7fbc
Andrea Pontillo <andreapontillo97@gmail.com>
2022-04-22 14:34:44 +0200

    stesura iniziale

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <HeaderComp @funzioneRicerca="metodoSearch" />
+    <div>{{ multimediaList[1].title }}</div>
+  </div>
+</template>
+
+<script>
+import "bootstrap";
+import axios from "axios";
+import HeaderComp from "./components/HeaderComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComp,
+  },
+  data() {
+    return {
+      apiKey: "c16fc230072b5fb47643e186e6d0d9ae",
+      multimediaList: [],
+      testoRicerca: "",
+    };
+  },
+
+  methods: {
+    metodoSearch(testo) {
+      this.testoRicerca = testo;
+    },
+  },
+
+  created() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=ritorno al futuro`
+      )
+      .then((res) => {
+        this.multimediaList = res.data.results;
+        console.log(this.multimediaList);
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "bootstrap/dist/css/bootstrap.min.css";
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <header class="d-flex justify-content-between align-items-center">
+    <div class="ms-3 text-danger text-uppercase fw-bold fs-2">Boolflix</div>
+    <div>
+      <form action="" class="me-5">
+        <input type="text" v-model="searchText" />
+        <button
+          type="submit"
+          class="btn btn-primary ms-4"
+          @click.prevent="$emit('funzioneRicerca', searchText)"
+        >
+          Ricerca
+        </button>
+      </form>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComp",
+  props: {},
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  background-color: black;
+  height: 100px;
+}
+</style>
